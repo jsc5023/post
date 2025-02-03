bc7f314db4cbc6a33caff3362fd6609acc5e3cac
SeongCheol Jung <kir5121@naver.com>
2025-02-03 09:40:37 +0900

    [feat] - restApi의 기본 틀을 가지고옴

---
+++ src/main/java/com/example/auth/domain/post/post/dto/PostDto.java
@@ -0,0 +1,40 @@
+package com.example.auth.domain.post.post.dto;
+
+import com.example.auth.domain.post.post.entity.Post;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostDto {
+
+    private long id;
+    @JsonProperty("createdDatetime")
+    private LocalDateTime createdDate;
+    @JsonProperty("modifiedDateTime")
+    private LocalDateTime modifiedDate;
+    private String title;
+    private String content;
+    private long authorId;
+    private String authorName;
+
+    public PostDto(Post post) {
+        this.id = post.getId();
+        this.createdDate = post.getCreatedDate();
+        this.modifiedDate = post.getModifiedDate();
+        this.title = post.getTitle();
+        this.content = post.getContent();
+        this.authorId = post.getAuthor().getId();
+        this.authorName = post.getAuthor().getNickname();
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public LocalDateTime getModifiedDate() {
+        return modifiedDate;
+    }
+
+}
