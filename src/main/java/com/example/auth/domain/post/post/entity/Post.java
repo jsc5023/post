fa4dc4f881dade58c2b9851cbf118046a999e312
SeongCheol Jung <kir5121@naver.com>
2025-02-04 23:58:14 +0900

    Comment를 연결할 수 있도록 추가

--- src/main/java/com/example/auth/domain/post/post/entity/Post.java
+++ src/main/java/com/example/auth/domain/post/post/entity/Post.java
@@ -1,10 +1,13 @@
 package com.example.auth.domain.post.post.entity;
 import com.example.auth.domain.member.member.entity.Member;
+import com.example.auth.domain.post.comment.entity.Comment;
 import com.example.auth.global.entity.BaseTime;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.ManyToOne;
+import com.example.auth.global.exception.ServiceException;
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @AllArgsConstructor
@@ -18,4 +21,35 @@ public class Post extends BaseTime {
     private Member author;
     private String title;
     private String content;
+
+    @OneToMany(mappedBy = "post", cascade = {CascadeType.PERSIST, CascadeType.REMOVE}, orphanRemoval = true)
+    @Builder.Default
+    private List<Comment> comments = new ArrayList<>();
+
+    public Comment addComment(Member author, String content) {
+        Comment comment = Comment
+                .builder()
+                .post(this)
+                .author(author)
+                .content(content)
+                .build();
+
+        comments.add(comment);
+
+        return comment;
+    }
+
+    public Comment getCommentById(long id) {
+
+        return comments.stream()
+                .filter(comment -> comment.getId() == id)
+                .findFirst()
+                .orElseThrow(
+                        () -> new ServiceException("404-2", "존재하지 않는 댓글입니다.")
+                );
+    }
+
+    public void deleteComment(Comment comment) {
+        comments.remove(comment);
+    }
 }
