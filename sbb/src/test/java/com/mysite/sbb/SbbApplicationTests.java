603d3a15914b486a839835eb73c53506e129055f
SeongCheol Jung <kir5121@naver.com>
2025-02-16 21:58:03 +0900

    타임리프를 이용한 게시판만들기

---
+++ sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -0,0 +1,37 @@
+package com.mysite.sbb;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.mysite.sbb.answer.Answer;
+import com.mysite.sbb.answer.AnswerRepository;
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.QuestionRepository;
+import com.mysite.sbb.question.QuestionService;
+
+@SpringBootTest
+class SbbApplicationTests {
+
+	@Autowired
+	private QuestionService questionService;
+
+	@Test
+	void testJpa() {
+		for(int i = 1; i <= 300; i++) {
+			String subject = String.format("테스트 데이터입니다:[%03d]", i);
+			String content = "내용무";
+			this.questionService.create(subject, content, null);
+		}
+	}
+
+}
